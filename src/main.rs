6375dd0c402c332378e1022592bbeef5d9fdd4c5
Paul Razvan Berg <hello@paulrberg.com>
2023-01-01 14:48:50 +0200

    feat: add serde serialization example
    build: add "serde" and "serde_json" as dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let point = Point { x: 1, y: 2 };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("Serialized point = {}", serialized);
 }
 
 #[cfg(test)]
